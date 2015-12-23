d98a756d8869df0f60ec1b459430e68d5166de9e
Mike <mgrahamjo@gmail.com>
2015-12-22 20:54:32 -0800

    add preview route

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ require('breadbox')({
 		'/breadbox': resolve => { resolve({}, 'breadbox/index.mnla'); },
 		'/write|authenticate': require('./controllers/write'),
 		'/write/{{id}}|authenticate': require('./controllers/edit'),
+		'/preview/{{folder}}/{{image}}': require('./controllers/preview')
 	}
 });
