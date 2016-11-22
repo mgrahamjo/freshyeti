7830a782a1d5f739a53f6fca5afd0a30924e13f2
Mike <mgrahamjo@gmail.com>
2016-11-21 21:42:51 -0800

    geo photo post

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('breadbox')({
-	controllers: {
-		'/index': require('./controllers/index'),
-		'/blog': require('./controllers/blog'),
-		'/post/<:id:>': require('./controllers/post'),
-		'/breadbox': resolve => { resolve({}, 'breadbox/index.mnla'); },
-		'/write|authenticate': require('./controllers/write'),
-		'/write/<:id:>|authenticate': require('./controllers/edit')
-	}
+  controllers: {
+    '/index': require('./controllers/index'),
+    '/blog': require('./controllers/blog'),
+    '/post/<:id:>': require('./controllers/post'),
+    '/breadbox': resolve => {
+      resolve({}, 'breadbox/index.mnla');
+    },
+    '/write|authenticate': require('./controllers/write'),
+    '/write/<:id:>|authenticate': require('./controllers/edit')
+  }
 });
